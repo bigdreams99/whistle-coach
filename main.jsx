5aadd21efaab99478593dd0354e4c07197e059bc
bigdreams99 <d.spinkstx@gmail.com>
2026-04-22 14:17:40 -0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import WhistleApp from './whistle-app.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WhistleApp />
+  </React.StrictMode>,
+)
